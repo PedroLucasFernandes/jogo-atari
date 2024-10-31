5415fbff8519e88586914047b4d715c2e3623a1c
aurelio-santana <aurelio.sg3@outlook.com>
2024-10-31 16:46:06 -0300

    Contexto de usuário e rotas do game no front

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,50 @@
+export type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'OPTIONS' | 'HEAD';
+
+export interface requestOptions {
+    url: string;
+    method: HTTPMethod;
+    body?: object | FormData;
+    formData?: boolean;
+    headers?: { [key: string]: string };
+    auth?: boolean;
+    socketId?: string;
+}
+
+export default async function request<T>({
+    url,
+    method,
+    body,
+    formData,
+    auth = true,
+    socketId,
+}: requestOptions): Promise<{
+    data: T;
+    error: boolean;
+    success: boolean;
+    status: number;
+    message?: string | string[];
+}> {
+
+    const requestOptions: RequestInit = {
+        method,
+        body: !formData ? JSON.stringify(body) : (body as FormData),
+        headers: {
+            ...(!formData ? { "Content-type": "application/json" } : {}),
+            ...(socketId ? { "x-socket-id": socketId } : {}),
+        },
+        cache: "no-store",
+        credentials: 'include',
+
+    };
+
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+
+    return {
+        data: data.data,
+        error: !response.ok,
+        success: data.success,
+        status: response.status,
+        message: data.message ? data.message : null,
+    };
+}
