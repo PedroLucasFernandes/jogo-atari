3d3697989fa4e76c28519aa5c70286bf87d1016a
Pedro Lucas Fernandes <plff201100@gmail.com>
2024-10-31 16:33:36 -0300

    feat: add gameRouter and Player Movement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Game from './components/Game';
 import LoginButton from './components/LoginButton';
 
 function App() {
-  const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-
-  if (!clientId) {
-    throw new Error("REACT_APP_GOOGLE_CLIENT_ID is not defined in .env file");
-  }
-
-  return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <LoginButton />
-    </GoogleOAuthProvider>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<LoginButton/>} />
+                <Route path="/game" element={<Game />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
