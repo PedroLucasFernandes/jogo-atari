2180907da603f13107046582b450d00a707729b6
Maiza Ferreira <maizafcontato@gmail.com>
2024-11-04 11:10:36 -0300

    fix/style: pagina login

--- frontend/src/views/Login/Login.tsx
+++ frontend/src/views/Login/Login.tsx
@@ -1,11 +1,14 @@
-import './Login.css'
+import './Login.css';
 import { useState } from "react";
 import { loginApi } from "../../api/loginApi";
 import { useUser } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import { User } from "../../interfaces/User";
+import character from '../../assets/character.png'
 import LoginGoogleButton from '../../components/LoginGoogleButton/LoginGoogleButton';
-
+import CustomInput from '../../components/CustomInput/CustomInput';
+import { toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css'; 
 
 export const Login: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -14,42 +17,51 @@ export const Login: React.FC = () => {
   const navigate = useNavigate();
 
   const handleLogin = async () => {
-    const { data, error } = await loginApi({ email, password });
-
-    if (error) {
-      //TODO: Alerta
-      console.error(error);
+  
+    if (!email || !password) {
+      toast.error("Por favor, preencha todos os campos.");
       return;
     }
-
+  
+    const { data, error } = await loginApi({ email, password });
+  
+    if (error) {
+      console.error("Erro da API:", error);
+      toast.error("Erro ao fazer login. Verifique seu e-mail e senha.");
+      return;
+    }
+  
     if (data) {
-      console.log("Logado com sucesso:", data);
+      toast.success("Logado com sucesso!");
       setUser(data as User);
       navigate('/monolito');
-
     }
   };
+  
 
   return (
     <div id="login">
+      <img src={character} alt="character" id="character" />
+      <div id='square'>
       <h1>Login</h1>
-      <input
+      <CustomInput
         placeholder='Email'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <input
+      <CustomInput
         placeholder='Password'
         value={password}
         onChange={(e) => setPassword(e.target.value)}
+        type='password'
       />
 
-      <button
+      <button className='button'
         disabled={!email || !password}
         onClick={handleLogin}
       >Entrar</button>
-    <LoginGoogleButton/>
+      <LoginGoogleButton />
+      </div>
     </div>
   );
-
 }
