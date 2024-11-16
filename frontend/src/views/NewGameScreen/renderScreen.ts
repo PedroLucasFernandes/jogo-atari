d47d3562627707ae0ed04e3252917877ca3011c6
Aurelio <aurelio.sg3@outlook.com>
2024-11-15 23:19:18 -0300

    feat: Utilizando apenas inteiros para X e Y na... Utilizando apenas inteiros nas coordenadas após a interpolação para evitar cálculos extras nas redenrizações dos pixels.

--- frontend/src/views/NewGameScreen/renderScreen.ts
+++ frontend/src/views/NewGameScreen/renderScreen.ts
@@ -6,7 +6,7 @@ interface IBallTrailPosition {
 }
 
 let ballTrailPositions: IBallTrailPosition[] = [];
-const TRAIL_LENGTH = 10; 
+const TRAIL_LENGTH = 10;
 
 export default function renderScreen(
   canvasScreen: HTMLCanvasElement,
@@ -36,7 +36,7 @@ export default function renderScreen(
   ballTrailPositions.forEach((position, index) => {
     const opacity = (index + 1) / ballTrailPositions.length;
     const radius = (gameState.ball.radius * (index + 1)) / ballTrailPositions.length;
-    
+
     context.beginPath();
     context.arc(position.x, position.y, radius, 0, Math.PI * 2);
     context.fillStyle = `rgba(255, 255, 255, ${opacity * 0.5})`;
@@ -44,16 +44,16 @@ export default function renderScreen(
   });
 
   const gradient = context.createRadialGradient(
-    gameState.ball.x, 
-    gameState.ball.y, 
+    gameState.ball.x,
+    gameState.ball.y,
     gameState.ball.radius * 0.5,
-    gameState.ball.x, 
-    gameState.ball.y, 
+    gameState.ball.x,
+    gameState.ball.y,
     gameState.ball.radius * 2
   );
   gradient.addColorStop(0, 'rgba(255, 255, 255, 0.8)');
   gradient.addColorStop(1, 'rgba(255, 255, 255, 0)');
-  
+
   context.beginPath();
   context.arc(gameState.ball.x, gameState.ball.y, gameState.ball.radius * 2, 0, Math.PI * 2);
   context.fillStyle = gradient;
@@ -71,12 +71,12 @@ export default function renderScreen(
       const image = planetImages[players[planet.ownerId].defendingPlanetId];
       const partWidth = planet.width / 2;
       const partHeight = planet.height / 3;
-  
+
       for (let i = 0; i < planet.parts.length; i++) {
         if (planet.parts[i]) {
           const partX = planet.x + (i % 2) * partWidth;
           const partY = planet.y + Math.floor(i / 2) * partHeight;
-  
+
           context.drawImage(
             image,
             (i % 2) * (image.width / 2),
@@ -89,12 +89,12 @@ export default function renderScreen(
             partHeight
           );
 
-        context.globalAlpha = 0.06; // Ajuste para o nível de transparência desejado
-        context.fillStyle = 'white'; // Ou uma cor ligeiramente mais clara
+          context.globalAlpha = 0.06; // Ajuste para o nível de transparência desejado
+          context.fillStyle = 'white'; // Ou uma cor ligeiramente mais clara
 
-        context.fillRect(partX + 1, partY + 1, partWidth - 2, partHeight - 2);
+          context.fillRect(partX + 1, partY + 1, partWidth - 2, partHeight - 2);
 
-        context.globalAlpha = 1.0;
+          context.globalAlpha = 1.0;
         }
       }
     }
@@ -107,41 +107,41 @@ export default function renderScreen(
 
     context.drawImage(playerImage, centerX, centerY, player.size, player.size);
 
-      // Adiciona o nome do jogador abaixo da imagem
-      context.font = '16px "Pixelify Sans", sans-serif'; // Tamanho da fonte do nome
-      context.fillStyle = 'white'; // Cor do nome
-      context.textAlign = 'center'; // Alinha o texto ao centro
-      context.textBaseline = 'top'; // Alinha o texto acima da linha de base (aqui fica logo abaixo da imagem)
-      context.fillText(player.username, centerX + player.size / 2, centerY + player.size); // Desenha o nome abaixo da imagem
+    // Adiciona o nome do jogador abaixo da imagem
+    context.font = '16px "Pixelify Sans", sans-serif'; // Tamanho da fonte do nome
+    context.fillStyle = 'white'; // Cor do nome
+    context.textAlign = 'center'; // Alinha o texto ao centro
+    context.textBaseline = 'top'; // Alinha o texto acima da linha de base (aqui fica logo abaixo da imagem)
+    context.fillText(player.username, centerX + player.size / 2, centerY + player.size); // Desenha o nome abaixo da imagem
   });
 
 
   // Função para interpolar entre a posição inicial e a posição alvo
-	function interpolatePlayer(interpolationFactor: number) {
-		if (!gameState) return;
+  function interpolatePlayer(interpolationFactor: number) {
+    if (!gameState) return;
 
-		Object.keys(gameState.players).forEach((playerId) => {
+    Object.keys(gameState.players).forEach((playerId) => {
       if (playerId === currentPlayerId) return;
-      
-			const player = gameState.players[playerId];
-			const interpolatedX = player.x + (player.toX - player.x) * interpolationFactor;
-			const interpolatedY = player.y + (player.toY - player.y) * interpolationFactor;
 
-			// Atualiza as posições de cada jogador no objeto interpolado
-			gameState.players[playerId] = {
-				...player,
-				x: interpolatedX,
-				y: interpolatedY,
-			};
-		});
-	}
+      const player = gameState.players[playerId];
+      const interpolatedX = Math.round(player.x + (player.toX - player.x) * interpolationFactor);
+      const interpolatedY = Math.round(player.y + (player.toY - player.y) * interpolationFactor);
+
+      // Atualiza as posições de cada jogador no objeto interpolado
+      gameState.players[playerId] = {
+        ...player,
+        x: interpolatedX,
+        y: interpolatedY,
+      };
+    });
+  }
 
   function interpolateBall(interpolationFactor: number) {
-		if (!gameState) return;
-    
+    if (!gameState) return;
+
     const ball = gameState.ball;
-    const interpolatedX = ball.x + (ball.toX - ball.x) * interpolationFactor;
-    const interpolatedY = ball.y + (ball.toY - ball.y) * interpolationFactor;
+    const interpolatedX = Math.round(ball.x + (ball.toX - ball.x) * interpolationFactor);
+    const interpolatedY = Math.round(ball.y + (ball.toY - ball.y) * interpolationFactor);
 
     // Atualiza as posições de cada jogador no objeto interpolado
     gameState.ball = {
@@ -149,5 +149,5 @@ export default function renderScreen(
       x: interpolatedX,
       y: interpolatedY,
     };
-	}
+  }
 }
